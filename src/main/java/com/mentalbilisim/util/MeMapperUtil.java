fde0f87c25ed2f3e2d6d0b69eb99a9b37dc1110c
Erhan Karakaya <erhan277@gmail.com>
2017-03-30 14:16:27 +0300

    Added getAllFields method

--- src/main/java/com/mentalbilisim/util/MeMapperUtil.java
+++ src/main/java/com/mentalbilisim/util/MeMapperUtil.java
@@ -1,7 +1,50 @@
 package com.mentalbilisim.util;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+import java.util.stream.Collectors;
+
 /**
  * author @er-han on 3/30/2017.
  */
 public class MeMapperUtil {
+  private static final Logger logger = LoggerFactory.getLogger(MeMapperUtil.class);
+
+  /**
+   * Gets all public, private, protected fields of the given type.
+   * If the given type has superclass, gets it's fields too using recursive call of self.
+   *
+   * @param type The type which's fields will be returned.
+   * @return List of the fields of the given type.
+   */
+  public static List<Field> getAllFields(Class<?> type) {
+    List<Field> fields = new ArrayList<>();
+    if (type.getSuperclass() != null) {
+      List<Field> fieldsOfSuper = getAllFields(type.getSuperclass());
+      fields.addAll(fieldsOfSuper);
+    }
+
+    List<Field> fieldsOfCurrent = Arrays.stream(type.getDeclaredFields())
+        .map(field -> {
+          Iterator<Field> iterator = fields.iterator();
+          while (iterator.hasNext()) {
+            Field tempField = iterator.next();
+            if (tempField.getName().equals(field.getName())) {
+              iterator.remove();
+              break;
+            }
+          }
+          return field;
+        }).collect(Collectors.toList());
+
+    fields.addAll(fieldsOfCurrent);
+
+    return fields;
+  }
 }
