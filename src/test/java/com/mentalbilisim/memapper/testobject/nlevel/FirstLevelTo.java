84a2b2f77212630634a77809db3d4202848ca11a
Erhan Karakaya <erhan277@gmail.com>
2017-03-31 11:57:03 +0300

    Added test object classes to use in testing

---
+++ src/test/java/com/mentalbilisim/memapper/testobject/nlevel/FirstLevelTo.java
@@ -0,0 +1,25 @@
+package com.mentalbilisim.memapper.testobject.nlevel;
+
+/**
+ * Created by erhan.karakaya on 3/31/2017.
+ */
+public class FirstLevelTo {
+  private Integer level1Integer;
+  private String level1String;
+
+  public Integer getLevel1Integer() {
+    return level1Integer;
+  }
+
+  public void setLevel1Integer(Integer level1Integer) {
+    this.level1Integer = level1Integer;
+  }
+
+  public String getLevel1String() {
+    return level1String;
+  }
+
+  public void setLevel1String(String level1String) {
+    this.level1String = level1String;
+  }
+}
---
+++ src/test/java/com/mentalbilisim/memapper/testobject/nlevel/SecondLevelTo.java
@@ -0,0 +1,25 @@
+package com.mentalbilisim.memapper.testobject.nlevel;
+
+/**
+ * Created by erhan.karakaya on 3/31/2017.
+ */
+public class SecondLevelTo extends FirstLevelTo {
+  private Integer level2Integer;
+  private String level2String;
+
+  public Integer getLevel2Integer() {
+    return level2Integer;
+  }
+
+  public void setLevel2Integer(Integer level2Integer) {
+    this.level2Integer = level2Integer;
+  }
+
+  public String getLevel2String() {
+    return level2String;
+  }
+
+  public void setLevel2String(String level2String) {
+    this.level2String = level2String;
+  }
+}
